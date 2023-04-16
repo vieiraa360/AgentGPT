fb68e800647213c3fb4f529e1150fb3b55703c94
Helder <vieiraa360@gmail.com>
2023-04-16 11:27:48 +0000

    update title

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -15,7 +15,7 @@ const DefaultLayout = (props: LayoutProps) => {
   return (
     <div className="flex flex-col bg-gradient-to-b from-[#2B2B2B] to-[#1F1F1F]">
       <Head>
-        <title>MyAutoGPT</title>
+        <title>AgentGPT</title>
         <meta name="description" content={description} />
         <meta name="twitter:site" content="@AgentGPT" />
         <meta name="twitter:card" content="summary_large_image" />
